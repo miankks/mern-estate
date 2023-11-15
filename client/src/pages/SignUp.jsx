002402cac7b33db9c9bf2cdb60532d2079a783b8
Bilal Jan <bilal.jan085@gmail.com>
2023-11-15 14:06:37 +0100

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -56,7 +56,7 @@ export default function SignUp() {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={'/signin'}>
-          <span className='text-blue-700'>Sign Up</span>
+          <span className='text-blue-700'>Sign In</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
